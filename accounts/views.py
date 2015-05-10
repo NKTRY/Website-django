cdb399480c3591257e355f5a6d33199f4ebe639e
albus12138 <albus.zly@gmail.com>
2015-05-10 21:26:01 +0800

    5-10 test send token by email

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from accounts.models import CustomUser
 # Create your views here.
+
+def get_token(request, username):
+    user = CustomUser.objects.filter(username=username)
+    if user != []:
+        # sendmail
+        return "success"
+    else:
+        return "failed"
--- nktc/urls.py
+++ nktc/urls.py
@@ -4,8 +4,8 @@ from __future__ import unicode_literals
 from django.conf.urls import patterns, include, url
 
 from accounts.admin import superadminsite, normaladminsite
+from accounts.views import get_token
 
-# 修改login页面url
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(normaladminsite.urls)),
     url(r'^superadmin/', include(superadminsite.urls)),
+    url(r'^superadmin/gettoken/(?P<username>\w+)/$', get_token, name="get_token"),
     url(r'^', include('frontend.urls')),
     url(r'^werobot/', include('wechat.urls', namespace='werobot')),
     url(r'^ueditor/',include('DjangoUeditor.urls')),
