4b14179d1e06b0aa3fa47cb9520fceb4477e78ec
albus12138 <albus.zly@gmail.com>
2015-05-15 23:22:13 +0800

    deploy version

--- nktc/settings.py
+++ nktc/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
     'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'NKTRY',
