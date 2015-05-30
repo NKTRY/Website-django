f8a27c3e80fd57ca659ce5d1d9e0ad9e8be21952
albus12138 <albus.zly@gmail.com>
2015-05-30 15:54:59 +0800

    5-30 Fix multihosts

--- nktc/hosts.py
+++ nktc/hosts.py
@@ -6,7 +6,7 @@ from accounts.admin import superadminsite, normaladminsite
 host_patterns = patterns('',
     host(r'www.test', 'frontend.urls', name='www'),
     host(r'test', 'frontend.urls', name='default'),
-    host(r'admin.test', 'accounts.surls', name='normal'),
-    host(r'nktc.test', 'accounts.nurls', name='super'),
+    host(r'admin.test', 'accounts.nurls', name='normal'),
+    host(r'nktc.test', 'accounts.surls', name='super'),
     host(r'werobot', 'wechat.urls', name='werobot')
 )
