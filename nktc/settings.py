92f4e55c504851ae9ba175a3c81240f8087fa28a
albus12138 <albus.zly@gmail.com>
2015-05-30 15:56:19 +0800

    5-30 Fix multihosts

--- nktc/settings.py
+++ nktc/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'NKTRY',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'mKcbsdhN29jd2',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
