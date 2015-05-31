d826a0145330e0989c5de18f9314c3c61b1e8350
albus12138 <albus.zly@gmail.com>
2015-05-31 13:14:24 +0800

    5-31

--- wechat/urls.py
+++ wechat/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django_werobot import make_view
-from wechat.robot import robot
+from wechat.views import werobot_view
 
 urlpatterns = patterns('',
-    url(r'^robot/', make_view(robot)),
+    url(r'^$', werobot_view, name='werobot_view'),
 )
