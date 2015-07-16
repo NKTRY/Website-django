127e43e6ce1584548fcd669014451e0e8e3376d2
albus12138 <albus.zly@gmail.com>
2015-07-16 20:56:57 +0800

    da~yue~yue~

--- frontend/models.py
+++ frontend/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import Permission
 from django.utils.translation import ugettext_lazy as _
