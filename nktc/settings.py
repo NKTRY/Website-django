ab4fd125edaf4eb93f1f1c7b218716fd0573eefe
albus12138 <albus.zly@gmail.com>
2015-07-18 16:41:41 +0800

    close debug

--- nktc/settings.py
+++ nktc/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '7o0&#mt)m$@b5=yyh3@s0@-zd$%3t^3d@#ipw$vef@yr)0yi%z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
