0d922b8e0fc934c31249a830796d19e4c5ad519a
albus12138 <albus.zly@gmail.com>
2015-07-26 12:52:57 +0800

    default

--- vote/models.py
+++ vote/models.py
@@ -11,13 +11,6 @@ from accounts.models import CustomUser
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="投票名称", max_length=20, unique=True)
-    description = models.CharField(verbose_name="投票介绍", max_length=100)
-    cd_time = models.IntegerField(verbose_name="投票等待时间[单位:s]", help_text="如不允许重复投票此项可填任意值")
-    max_vote = models.IntegerField(verbose_name="最多重复投票次数")
-    is_single_select = models.BooleanField(verbose_name="是否是单项选择")
-    max_choice = models.IntegerField(verbose_name="最多选项数", help_text="如选中单项选择此项可填任意值")
-    cover_img = models.ImageField(verbose_name="投票封面图片", upload_to="img/Vote", help_text="推荐尺寸: ---px*---px [其他尺寸请保持长宽比相同]")
-    author = models.ForeignKey(CustomUser, verbose_name="投票发起人")
     description = models.CharField(verbose_name="投票介绍", max_length=100, default="")
     cd_time = models.IntegerField(verbose_name="投票等待时间[单位:s]", default=60, help_text="如不允许重复投票此项可填任意值")
     max_vote = models.IntegerField(verbose_name="最多重复投票次数", default=1)
@@ -39,7 +32,6 @@ class Question(models.Model):
 class Choice(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="选项名称", max_length=20, unique=True)
-    description = models.CharField(verbose_name="描述", max_length=255)
     description = models.CharField(verbose_name="描述", max_length=255, blank=True)
     url = models.URLField(verbose_name="详细链接", blank=True)
     img = models.ImageField(verbose_name="选项图片", upload_to="img/Vote", blank=True, help_text="推荐尺寸: ---px*---px [其他尺寸请保持长宽比相同][可以选择不添加图片]")
@@ -56,8 +48,6 @@ class Choice(models.Model):
 class Vote(models.Model):
     id = models.AutoField(primary_key=True)
     timestamp = models.DateTimeField(verbose_name="时间戳", default=timezone.now)
-    ip = models.IPAddressField(verbose_name="IP地址")
-    address = models.CharField(verbose_name="物理地址", max_length=255)
     ip = models.IPAddressField(verbose_name="IP地址", default="0.0.0.0")
     address = models.CharField(verbose_name="物理地址", max_length=255, default="")
     choice = models.ForeignKey(Choice, verbose_name="所属选项")
@@ -73,9 +63,6 @@ class Vote(models.Model):
 
 class VoteUser(models.Model):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(verbose_name="用户名", max_length=20, unique=True)
-    password = models.CharField(verbose_name="密码", max_length=50)
-    changed_password = models.CharField(verbose_name="修改密码", max_length=20)
     username = models.CharField(verbose_name="用户名", max_length=20, unique=True, default="")
     password = models.CharField(verbose_name="密码", max_length=50, default="")
     changed_password = models.CharField(verbose_name="修改密码", max_length=20, default="")
