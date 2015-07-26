15392ce6a8844de97299ecf45343f2daaae3faba
albus12138 <albus.zly@gmail.com>
2015-07-26 12:48:39 +0800

    default

--- vote/models.py
+++ vote/models.py
@@ -11,7 +11,6 @@ from accounts.models import CustomUser
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="投票名称", max_length=20, unique=True)
-<<<<<<< HEAD
     description = models.CharField(verbose_name="投票介绍", max_length=100)
     cd_time = models.IntegerField(verbose_name="投票等待时间[单位:s]", help_text="如不允许重复投票此项可填任意值")
     max_vote = models.IntegerField(verbose_name="最多重复投票次数")
@@ -19,7 +18,6 @@ class Question(models.Model):
     max_choice = models.IntegerField(verbose_name="最多选项数", help_text="如选中单项选择此项可填任意值")
     cover_img = models.ImageField(verbose_name="投票封面图片", upload_to="img/Vote", help_text="推荐尺寸: ---px*---px [其他尺寸请保持长宽比相同]")
     author = models.ForeignKey(CustomUser, verbose_name="投票发起人")
-=======
     description = models.CharField(verbose_name="投票介绍", max_length=100, default="")
     cd_time = models.IntegerField(verbose_name="投票等待时间[单位:s]", default=60, help_text="如不允许重复投票此项可填任意值")
     max_vote = models.IntegerField(verbose_name="最多重复投票次数", default=1)
@@ -27,7 +25,6 @@ class Question(models.Model):
     max_choice = models.IntegerField(verbose_name="最多选项数", help_text="如选中单项选择此项可填任意值", default=1)
     cover_img = models.ImageField(verbose_name="投票封面图片", upload_to="img/Vote", help_text="推荐尺寸: ---px*---px [其他尺寸请保持长宽比相同]", default="")
     author = models.ForeignKey(CustomUser, verbose_name="投票发起人", default="")
->>>>>>> origin/dev-version
     start_date = models.DateField(verbose_name="开始时间")
     end_date = models.DateField(verbose_name="结束时间")
 
@@ -42,11 +39,8 @@ class Question(models.Model):
 class Choice(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="选项名称", max_length=20, unique=True)
-<<<<<<< HEAD
     description = models.CharField(verbose_name="描述", max_length=255)
-=======
     description = models.CharField(verbose_name="描述", max_length=255, blank=True)
->>>>>>> origin/dev-version
     url = models.URLField(verbose_name="详细链接", blank=True)
     img = models.ImageField(verbose_name="选项图片", upload_to="img/Vote", blank=True, help_text="推荐尺寸: ---px*---px [其他尺寸请保持长宽比相同][可以选择不添加图片]")
     question = models.ForeignKey(Question, verbose_name="所属投票")
@@ -62,13 +56,10 @@ class Choice(models.Model):
 class Vote(models.Model):
     id = models.AutoField(primary_key=True)
     timestamp = models.DateTimeField(verbose_name="时间戳", default=timezone.now)
-<<<<<<< HEAD
     ip = models.IPAddressField(verbose_name="IP地址")
     address = models.CharField(verbose_name="物理地址", max_length=255)
-=======
     ip = models.IPAddressField(verbose_name="IP地址", default="0.0.0.0")
     address = models.CharField(verbose_name="物理地址", max_length=255, default="")
->>>>>>> origin/dev-version
     choice = models.ForeignKey(Choice, verbose_name="所属选项")
     user = models.ForeignKey("VoteUser", verbose_name="投票用户")
 
@@ -82,15 +73,12 @@ class Vote(models.Model):
 
 class VoteUser(models.Model):
     id = models.AutoField(primary_key=True)
-<<<<<<< HEAD
     username = models.CharField(verbose_name="用户名", max_length=20, unique=True)
     password = models.CharField(verbose_name="密码", max_length=50)
     changed_password = models.CharField(verbose_name="修改密码", max_length=20)
-=======
     username = models.CharField(verbose_name="用户名", max_length=20, unique=True, default="")
     password = models.CharField(verbose_name="密码", max_length=50, default="")
     changed_password = models.CharField(verbose_name="修改密码", max_length=20, default="")
->>>>>>> origin/dev-version
     is_active = models.BooleanField(verbose_name="账户状态", default=True)
     openid = models.CharField(verbose_name="用户微信ID", max_length=255)
 
