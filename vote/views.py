c0771add80a5b578a8d700085694d74785f6ded1
albus12138 <albus.zly@gmail.com>
2015-07-29 19:02:20 +0800

    finish vote system

--- vote/views.py
+++ vote/views.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render, redirect
 from django.core.urlresolvers import reverse
 from django.contrib import messages
